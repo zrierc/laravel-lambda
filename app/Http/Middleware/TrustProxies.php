15a533ffbe69c319b226767291e92f0d2f979406
zrierc <68382685+zrierc@users.noreply.github.com>
2022-10-01 16:28:56 +0700

    Chore: Set proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = ['0.0.0.0/0', '2000:0:0:0:0:0:0:0/3'];
 
     /**
      * The headers that should be used to detect proxies.
