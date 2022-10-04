00f15c793524181330781650f1baa95afa2ec8bf
zrierc <68382685+zrierc@users.noreply.github.com>
2022-10-04 10:24:33 +0700

    Fix(config): Revert storage path to default

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,13 +32,13 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('/tmp/storage/framework/app'),
+            'root' => storage_path('app'),
             'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('/tmp/storage/framework/app/public'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
